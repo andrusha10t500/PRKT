b8206caa91a6c0257654ee800a1b761e6cd6eafe
andrusha10t500 <andrusha10t500@gmail.com>
2021-05-24 12:08:15 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+//-----Pattern Factory-----//
+//Seek in File
+abstract class SiF {
+
+    abstract function str() : Args ;
+
+    public function out() {
+
+        $find = $this->str();
+        $find->get_position();
+    }
+}
+
+//Arguments
+interface Args {
+
+    public function get_position() ;
+}
+
+//----Clents Classes----
+class find extends SiF {
+
+    private $path, $str;
+
+    public function __construct($p, $s)
+    {
+        $this->path = $p;
+        $this->str = $s;
+    }
+
+    function str(): Args
+    {
+        // TODO: Implement str() method.
+        return new find_string($this->path, $this->str);
+    }
+}
+
+class find_string implements Args {
+
+    private $path, $str;
+
+    public function __construct($p,$s)
+    {
+        $this->path = $p;
+        $this->str = $s;
+    }
+
+    public function get_position()
+    {
+        // TODO: Implement get_position() method.
+
+        if ( $file = fopen($this->path,"r") ) {
+
+            $string_number = 0;
+            $position = 0;
+            while(($line = fgets($file,4096)) !== false && !$position) {
+
+                $string_number++;
+                if(strpos($line,$this->str) !== false) {
+
+                    $position = strpos($line,$this->str);
+                }
+            }
+            echo 'номер строки: ' . $string_number . ', позиция: ' . ($position+1);
+
+        } else {
+
+            echo 'ничего не вышло';
+
+        }
+
+    }
+}
+
+//----Client Code----
+$result = new find("/var/www/html/file.txt", "wds");
+$result->out();
+
+?>
+
