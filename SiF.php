033916de0039e128b3e2c2fe18878c2d0748195c
andrusha10t500 <andrusha10t500@gmail.com>
2021-05-25 22:39:32 +1000

    edit SiF.php (PSR-12)

--- SiF.php
+++ SiF.php
@@ -2,27 +2,28 @@
 
 //-----Pattern Factory-----//
 //Seek in File
-abstract class SiF {
+abstract class SiF
+{
+    abstract function str(): Args ;
 
-    abstract function str() : Args ;
-
-    public function out() {
-
+    public function out()
+    {
         $find = $this->str();
         $find->get_position();
     }
 }
 
 //Arguments
-interface Args {
-
+interface Args
+{
     public function get_position() ;
 }
 
 //----Clents Classes----
-class find extends SiF {
-
-    private $path, $str;
+class find extends SiF implements Args
+{
+    private $path;
+    private $str;
 
     public function __construct($p, $s)
     {
@@ -33,51 +34,24 @@ class find extends SiF {
     function str(): Args
     {
         // TODO: Implement str() method.
-        return new find_string($this->path, $this->str);
-    }
-}
-
-class find_string implements Args {
-
-    private $path, $str;
-
-    public function __construct($p,$s)
-    {
-        $this->path = $p;
-        $this->str = $s;
+        return new find($this->path, $this->str);
     }
 
     public function get_position()
     {
         // TODO: Implement get_position() method.
-
-        if ( $file = fopen($this->path,"r") ) {
-
+        if ($file = fopen($this->path,"r")) {
             $string_number = 0;
             $position = 0;
             while(($line = fgets($file,4096)) !== false && !$position) {
-
                 $string_number++;
                 if(strpos($line,$this->str) !== false) {
-
                     $position = strpos($line,$this->str);
                 }
             }
             echo 'номер строки: ' . $string_number . ', позиция: ' . ($position+1);
-
         } else {
-
             echo 'ничего не вышло';
-
         }
-
     }
 }
-
-//----Client Code----
-//$result = new find("file.txt", "wds");
-//$result->out();
-//result:
-//номер строки: 7, позиция: 4
-?>
-
